693652ef9189b52e64cdb1974e8042ceccec84a7
baonhanthai2710 <baonhanthai2710@gmail.com>
2025-05-23 14:29:50 +0700

    fix: Refactor array state

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,33 @@
+import React from "react"
+
 export default function Main() {
+    const [ingredients, setIngredients] = React.useState([])
+    
+    const ingredientsListItems = ingredients.map((ingredient, index) => {
+        return <li key={index}>{ingredient}</li>
+    })
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+    }
+
     return (
         <main>
-            <form className="add-ingredient-form">
+            <form onSubmit={handleSubmit} className="add-ingredient-form">
                 <input 
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
+                    name="ingredient"
                 />
-                <button>Add ingredient</button>
+                <button >Add ingredient</button>
             </form>
+            <ul>
+                {ingredientsListItems}
+            </ul>
         </main>
     )
 }
